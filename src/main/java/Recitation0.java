a7a070da653cb10a1d5b07263742c3d9b3dcd795
seangilliam <seanmgilliam11@gmail.com>
2026-01-24 08:16:06 -0500

    Printed Hello World

--- src/main/java/Recitation0.java
+++ src/main/java/Recitation0.java
@@ -1,5 +1,6 @@
 public class Recitation0 {
-    public static void main(String [] args) {
-        System.out.println();
+    public static void main(String [] args) {System.out.println();
+    System.out.println("Hello World!");
     }
+
 }
